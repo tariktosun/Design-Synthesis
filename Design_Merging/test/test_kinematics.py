cda9c2d760d3899bb7891b00f468830adcbab970
Tarik Tosun <ttosun@gmail.com>
2014-01-17 22:25:05 -0500

    First commit in kinematics branch.  Added test_kinematics

---
+++ Design_Merging/test/test_kinematics.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 17, 2014
+
+@author: tarik
+'''
+import unittest
+
+
+class Test_Kinematics(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
