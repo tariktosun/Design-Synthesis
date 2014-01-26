f189aa68d8e2989c92824d641d2d26d0bb1197d3
Tarik Tosun <ttosun@gmail.com>
2014-01-25 23:16:27 -0500

    New branch: new_kinematics.  Added test_new_kinematics.py.  Nothing in it yet.

---
+++ Design_Merging/test/test_new_kinematics.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 25, 2014
+
+@author: tarik
+'''
+import unittest
+
+
+class Test_new_kinematics(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
