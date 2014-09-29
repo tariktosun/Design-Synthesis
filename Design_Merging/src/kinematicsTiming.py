8e2872aa44c0fcb3fcca626736154d3f574c3ffc
Tarik <tarik@modlab-vision-2.(none)>
2014-09-29 12:59:12 -0400

    added times_40_100.txt

---
+++ Design_Merging/src/kinematicsTiming.py
@@ -0,0 +1,38 @@
+'''
+Script for random tree timing analysis.
+Created on Feb 1, 2014
+
+@author: tarik
+'''
+import sys
+sys.path.append('..')
+import Embedding.Node as Node
+import Embedding.Design as Design
+from Embedding import Embedding
+from smoresRandomDesign import SmoresRandomDesign
+import time
+import numpy as np
+from matplotlib.pyplot import *
+from math import sqrt
+import pdb
+
+
+
+# Create parameters:
+types_subsumed = {1: [1], 2: [2]}
+length_scaling = 1
+params = {'types_subsumed': types_subsumed,
+                   'length_scaling': length_scaling}
+subN = 40 
+superN = 100 
+
+subD = SmoresRandomDesign(2,subN, sqrt(subN)/2, is_subdesign=True)
+superD = SmoresRandomDesign(2, superN, sqrt(superN)/2)
+embedding = Embedding.Embedding(superD, subD, params)
+start = time.time()
+print('Beginning to check embedding with ' + str(subN) + ' modules in subdesign and ' + str(superN) + ' modules in superdesign.')
+succeeds = embedding.check_kinematic_embedding_dynamic(record_timings=True)
+t = time.time() - start
+print('Finished in ' + str(t) + ' seconds.')
+embedding.write_timings_to_file('times_40_100.txt')
+
